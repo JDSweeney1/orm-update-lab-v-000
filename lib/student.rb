1e19820559a899412f390d75148bbdf394eda580
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 01:55:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,15 @@ def initialize(id = nil, name, grade)
   @id, @name, @grade = id, name, grade
 end
 
+def self.create_table
+  sql = <<-SQL
+    INSERT TABLE students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade INTEGER
+    )
+  SQL
 
-
+  DB[:conn].execute(sql)
+end
 end
