29d2f9f69dcaea856875b9fb85d8beb91e1fb36b
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 01:55:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,19 +5,19 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
 
-def initialize(id = nil, name, grade)
-  @id, @name, @grade = id, name, grade
-end
+  def initialize(id = nil, name, grade)
+    @id, @name, @grade = id, name, grade
+  end
 
-def self.create_table
-  sql = <<-SQL
-    INSERT TABLE students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade INTEGER
-    )
-  SQL
+  def self.create_table
+    sql = <<-SQL
+      INSERT TABLE students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
 
-  DB[:conn].execute(sql)
-end
+    DB[:conn].execute(sql)
+  end
 end
