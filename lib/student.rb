03db92322dcede174f671dff6de1542ed51727fd
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 01:53:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,9 @@ require_relative "../config/environment.rb"
 
 class Student
 
+  attr_accessor :name, :grade
+  attr_reader :id
+
 def initialize(id = nil, name, grade)
   @id, @name, @grade = id, name, grade
 end
