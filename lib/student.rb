e1048a5e40f91c3aa8ed02b89446861f013074b5
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 01:55:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-      INSERT TABLE students (
+      CREATE TABLE students (
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
