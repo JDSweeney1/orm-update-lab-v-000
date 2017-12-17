5f50601206830554c3fe030fe5f7a298e3d0cb14
Jered Sweeney <sweeneyjered@gmail.com>
2017-12-17 02:13:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -41,6 +41,18 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+
+  def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
   end
 
 
